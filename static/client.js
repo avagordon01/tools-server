ff2a4c5681a903ed173fe2e2c3add48a4fbae4e9
Patrick Gordon <pgpatrickgordon@gmail.com>
2019-08-12 22:35:39 +0100

    multiple harness support on frontend

--- static/client.js
+++ static/client.js
@@ -1,36 +1,51 @@
-let ws = new WebSocket("ws://localhost:9999", "lws-minimal-server-echo");
+let ws = new WebSocket("ws://localhost:9999", "lws-minimal-server-echo")
 
 ws.onopen = function(e) {
-    console.log("[open] Connection established");
+    console.log("[open] Connection established")
     ws.binaryType = "arraybuffer"
-};
+}
 
+function create_node_from_template(template_id, container_id, new_node_id) {
+    var template = document.getElementById(template_id)
+    var container = document.getElementById(container_id)
+    var clone = document.importNode(template.content, true)
+    container.appendChild(clone)
+    container.lastElementChild.id = new_node_id
+    return document.getElementById(new_node_id)
+}
+
+metadata = {}
 ws.onmessage = function(event) {
     if (event.data instanceof Blob || event.data instanceof ArrayBuffer) {
+        var cli = document.getElementById('cli-' + metadata.pid)
+        if (cli == null) {
+            cli = create_node_from_template('cli-template', 'cli-container', 'cli-' + metadata.pid)
+            cli.pid.value = metadata.pid
+            cli.onsubmit = function(event) {
+                var cli = event.target
+                input = cli.input.value + '\n'
+                cli.input.value = ''
+                cli.output.value += input
+                ws.send(input)
+                event.preventDefault()
+                return false
+            }
+        }
         let dec = new TextDecoder()
-        document.forms.cli.output.value += dec.decode(event.data)
+        cli.output.value += dec.decode(event.data)
     } else {
-        console.log(event.data)
+        metadata = JSON.parse(event.data)
     }
-};
+}
 
 ws.onclose = function(event) {
     if (event.wasClean) {
-        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
     } else {
-        console.log('[close] Connection died');
+        console.log('[close] Connection died')
     }
-};
+}
 
 ws.onerror = function(error) {
-    console.log(`[error] ${error.message}`);
-};
-
-document.forms.cli.onsubmit = function(event) {
-    input = document.forms.cli.input.value + '\n'
-    document.forms.cli.output.value += input
-    ws.send(input)
-    document.forms.cli.input.value = ''
-    event.preventDefault();
-    return false;
+    console.log(`[error] ${error.message}`)
 }
