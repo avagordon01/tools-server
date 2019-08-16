916e49877b483cd5080fc23a63951d58c371587d
Patrick Gordon <pgpatrickgordon@gmail.com>
2019-08-16 23:20:00 +0100

    factor out harness into function

--- harness.cc
+++ harness.cc
@@ -1,41 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include "tcp.hh"
+#include <stdlib.h>
 
-#include "protocol-internal.hh"
+#include "harness.hh"
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "usage: %s pid\n", argv[0]);
         return 1;
     }
-    char tool[] = "gdb";
-    char *pid_text = argv[1];
-    char *tool_args[] = {"gdb", "-q", "-p", pid_text, NULL};
-
-    int in_fd = aether::tcp::connect_to_host_port_with_timeout("127.1", "19000", 10);
-    int out_fd = aether::tcp::connect_to_host_port_with_timeout("127.1", "19000", 10);
-    int err_fd = aether::tcp::connect_to_host_port_with_timeout("127.1", "19000", 10);
-
-    uint64_t pid = atoi(pid_text);
-
-    connect_message m {};
-    m.tool_id = tools::gdb;
-    m.pid = pid;
-    m.stream_id = streams::in;  send(in_fd, &m, sizeof(m), 0);
-    m.stream_id = streams::out; send(out_fd, &m, sizeof(m), 0);
-    m.stream_id = streams::err; send(err_fd, &m, sizeof(m), 0);
-
-    dup2(in_fd, STDIN_FILENO);
-    dup2(out_fd, STDOUT_FILENO);
-    dup2(err_fd, STDERR_FILENO);
-
-    close(in_fd);
-    close(out_fd);
-    close(err_fd);
-
-    execvp(tool, tool_args);
+    uint64_t pid = atoi(argv[1]);
+    harness(tools::gdb, pid);
 }
---
+++ harness.hh
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "tcp.hh"
+
+#include "protocol-internal.hh"
+
+void harness(tools tool, int pid) {
+    int in_fd = aether::tcp::connect_to_host_port_with_timeout("127.1", "19000", 10);
+    int out_fd = aether::tcp::connect_to_host_port_with_timeout("127.1", "19000", 10);
+    int err_fd = aether::tcp::connect_to_host_port_with_timeout("127.1", "19000", 10);
+
+    connect_message m {};
+    m.tool_id = tool;
+    m.pid = pid;
+    m.stream_id = streams::in;  send(in_fd, &m, sizeof(m), 0);
+    m.stream_id = streams::out; send(out_fd, &m, sizeof(m), 0);
+    m.stream_id = streams::err; send(err_fd, &m, sizeof(m), 0);
+
+    dup2(in_fd, STDIN_FILENO);
+    dup2(out_fd, STDOUT_FILENO);
+    dup2(err_fd, STDERR_FILENO);
+
+    close(in_fd);
+    close(out_fd);
+    close(err_fd);
+
+    char pid_text[32];
+    sprintf(pid_text, "%d", pid);
+
+    char* tool_command;
+    char** tool_args;
+    char *gdb_args[] = {"gdb", "-q", "-p", pid_text, NULL};
+    switch (tool) {
+        case tools::gdb:
+            tool_command = "gdb";
+            tool_args = gdb_args;
+            break;
+    }
+    execvp(tool_command, tool_args);
+}
