6f17cda6fbb14cbce83576abd4e22a084d18eacf
Patrick Gordon <pgpatrickgordon@gmail.com>
2019-08-17 13:52:00 +0100

    harness program accept multiple pids

--- harness.cc
+++ harness.cc
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 #include "harness.hh"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s pid\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s pid0 pid1 ...\n", argv[0]);
         return 1;
     }
-    uint64_t pid = atoi(argv[1]);
-    harness(tools::gdb, pid);
+    for (size_t i = 1; i < argc; i++) {
+        uint64_t pid = atoi(argv[i]);
+        harness(tools::gdb, pid);
+    }
+
+    waitpid(-1, NULL, 0);
 }
